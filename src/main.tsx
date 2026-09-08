09f13ecf8c2a75d7e69cfcfbea5b6d0c49cbe4e6
SmetchikNN <smetchiknn@gmail.com>
2026-09-08 13:25:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,3 @@
---- src/main.tsx (原始)
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-
-
-+++ src/main.tsx (修改后)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
